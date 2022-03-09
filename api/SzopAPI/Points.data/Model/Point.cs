5e4423e06c829b4352f3ff669d9a2beff7aa3398
Matyjash <lengom4w@gmail.com>
2022-03-09 22:15:59 +0100

    Finding nearby points by API
    Test version of finding nearby (temporary 5km) points to given localization.

--- api/SzopAPI/Points.data/Model/Point.cs
+++ api/SzopAPI/Points.data/Model/Point.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace API.data.Model
 {
@@ -11,9 +7,12 @@ namespace API.data.Model
         public int PointId { get; set; }
         public string Street { get; set; }
         public int Number { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
         public TimeSpan OpeningHour { get; set; }
         public TimeSpan ClosingHour { get; set; }
         public DateTime Date { get; set; }
+
 
     }
 }
--- api/SzopAPI/Points.data/Repository/PointsRepository.cs
+++ api/SzopAPI/Points.data/Repository/PointsRepository.cs
@@ -9,8 +9,8 @@ namespace API.data.Repository
     {
         List<Point> points = new List<Point>
         {
-            new Point{PointId=1, Street="Grunwald", Number = 2, OpeningHour = new TimeSpan(13,20,00) , ClosingHour = new TimeSpan(13,20,00), Date = DateTime.Today},
-            new Point{PointId=1, Street="Rynek", Number = 3, OpeningHour = new TimeSpan(13,20,00) , ClosingHour = new TimeSpan(13,20,00), Date = DateTime.Today},
+            new Point{PointId=1, Street="Czajkowskiego 39 A", Number = 2,latitude=51.14011389588952, longitude = 17.066555586778556, OpeningHour = new TimeSpan(13,20,00) , ClosingHour = new TimeSpan(13,20,00), Date = DateTime.Today},
+            new Point{PointId=2, Street="Rynek", Number = 3, latitude = 51.1394393, longitude = 17.0226578, OpeningHour = new TimeSpan(13,20,00) , ClosingHour = new TimeSpan(13,20,00), Date = DateTime.Today},
         };
         public List<Point> GetAllPoints()
         {
