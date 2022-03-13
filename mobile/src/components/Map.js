cf13a569debd237e321f9e63ae893f60c883cc20
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2022-03-13 17:20:41 +0100

    Extract Marker component from Map component

--- mobile/src/components/Map.js
+++ mobile/src/components/Map.js
@@ -1,39 +1,24 @@
-import {View, Text} from 'react-native';
 import React from 'react';
-import MapView, {Marker} from 'react-native-maps';
+import MapView from 'react-native-maps';
 import styled from 'styled-components';
-import {tintColor} from '../constants/colors';
+import SzopMarker from './SzopMarker';
 
-//#region styles
 const StyledMapView = styled(MapView)`
   flex: 1;
 `;
 
-//#endregion
-
 const Map = ({points}) => {
-  const renderMarkers = () =>
-    points.map(point => (
-      <Marker
-        key={point.point_id}
-        title={point.title}
-        description={point.desc}
-        coordinate={{
-          latitude: Number.parseFloat(point.lat),
-          longitude: Number.parseFloat(point.lng),
-        }}
-        pinColor={tintColor}
-      />
-    ));
+  const initialRegion = {
+    latitude: 51.11108,
+    longitude: 17.033686,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  const renderMarkers = () => points.map(point => <SzopMarker point={point} />);
 
   return (
-    <StyledMapView
-      initialRegion={{
-        latitude: 51.11108,
-        longitude: 17.033686,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}>
+    <StyledMapView initialRegion={initialRegion}>
       {renderMarkers()}
     </StyledMapView>
   );
