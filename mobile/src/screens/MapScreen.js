91a1e4bde41acb09ada1ab12c466848d1cf7d216
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2022-03-13 14:46:43 +0100

    Add Map

--- mobile/src/screens/MapScreen.js
+++ mobile/src/screens/MapScreen.js
@@ -1,25 +1,19 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {LanguageContext} from '../context/LanguageContextProvider';
+import Map from '../components/Map';
+
+export const mapScreenName = 'MapScreen';
 
 const MapScreen = () => {
   const [shouldLoadMap, setShouldLoadMap] = useState(false);
+
+  const {text} = useContext(LanguageContext);
+
   return shouldLoadMap ? (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <Map />
   ) : (
-    <TouchableOpacity onPress={() => setShouldLoadMap(true)}>
-      <View
-        style={{
-          backgroundColor: 'green',
-          margin: 8,
-          padding: 4,
-          borderRadius: 5,
-          elevation: 4,
-        }}>
-        <Text style={{color: 'white'}}>Wczytaj mape</Text>
-      </View>
-    </TouchableOpacity>
+    <Button title={text.loadTheMap} onPress={() => setShouldLoadMap(true)} />
   );
 };
 
