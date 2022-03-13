f1fc861724e15220121a4b91a1536a0262a591ca
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2022-03-13 17:07:34 +0100

    Add markers to map

--- mobile/src/components/Map.js
+++ mobile/src/components/Map.js
@@ -1,21 +1,41 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import styled from 'styled-components';
+import {tintColor} from '../constants/colors';
 
-import MapView from 'react-native-maps';
+//#region styles
+const StyledMapView = styled(MapView)`
+  flex: 1;
+`;
 
-const Map = () => {
+//#endregion
+
+const Map = ({points}) => {
+  const renderMarkers = () =>
+    points.map(point => (
+      <Marker
+        key={point.point_id}
+        title={point.title}
+        description={point.desc}
+        coordinate={{
+          latitude: Number.parseFloat(point.lat),
+          longitude: Number.parseFloat(point.lng),
+        }}
+        pinColor={tintColor}
+      />
+    ));
+
   return (
-    <MapView
-      style={{
-        flex: 1,
-      }}
+    <StyledMapView
       initialRegion={{
         latitude: 51.11108,
         longitude: 17.033686,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
-      }}
-    />
+      }}>
+      {renderMarkers()}
+    </StyledMapView>
   );
 };
 
--- mobile/src/screens/MapScreen.js
+++ mobile/src/screens/MapScreen.js
@@ -1,6 +1,7 @@
 import {View, Text, Button} from 'react-native';
 import React, {useContext, useState} from 'react';
 import {LanguageContext} from '../context/LanguageContextProvider';
+import {PointsContext} from '../context/PointsContextProvider';
 import Map from '../components/Map';
 
 export const mapScreenName = 'MapScreen';
@@ -9,9 +10,10 @@ const MapScreen = () => {
   const [shouldLoadMap, setShouldLoadMap] = useState(false);
 
   const {text} = useContext(LanguageContext);
+  const {points} = useContext(PointsContext);
 
   return shouldLoadMap ? (
-    <Map />
+    <Map points={points} />
   ) : (
     <Button title={text.loadTheMap} onPress={() => setShouldLoadMap(true)} />
   );
