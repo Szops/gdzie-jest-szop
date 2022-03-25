6c22f4a230f9531f95817cb5034998999431eb35
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2022-03-25 19:23:48 +0100

    Add connection to API server

--- mobile/src/api/szopPoints.js
+++ mobile/src/api/szopPoints.js
@@ -1,8 +1,27 @@
 import {Alert} from 'react-native';
-import * as data from './testRespons.json';
+
+const host = `https://gdzie-jest-szop-api.herokuapp.com`;
+
+export const getVersion = async () => {
+  return await fetch(`${host}/version`)
+    .then(res => {
+      if (!res.ok) throw Error(`Couldn't fetch data from that source!`);
+      return res.json();
+    })
+    .catch(err => {
+      Alert.alert('ERROR', err.message);
+      console.log(err.message);
+    });
+};
 
 export const getPoints = async () => {
-  const points = data.markers;
-
-  return points;
+  return await fetch(`${host}/points`)
+    .then(res => {
+      if (!res.ok) throw Error(`Couldn't fetch data from that source!`);
+      return res.json();
+    })
+    .catch(err => {
+      Alert.alert('ERROR', err.message);
+      console.log(err.message);
+    });
 };
