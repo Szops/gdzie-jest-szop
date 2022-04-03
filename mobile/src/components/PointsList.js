f0ba91357458d1e9ca3b3d82b2990879af1cf0e9
StawKarpi <stanislaw.karpisiak@gmail.com>
2022-04-03 22:57:14 +0200

    Added sections based on sectors to the points list

--- mobile/src/components/PointsList.js
+++ mobile/src/components/PointsList.js
@@ -1,19 +1,61 @@
-import {FlatList} from 'react-native';
+import {SectionList} from 'react-native';
+import {HugeText} from './Text';
 import React from 'react';
 import PointListItem from './PointListItem';
 
 export default function PointsList({points, searchPhrase}) {
   const renderItem = item => <PointListItem address={item.street} />;
-  return (
-    <FlatList
-      data={points.filter(p =>
-        String(p.street.toLowerCase()).includes(
-          String(searchPhrase.toLowerCase()),
+
+  const data = [
+    {
+      title: 'Stare Miasto i Śródmieście',
+      data: points
+        .filter(p => p.sector.includes('1'))
+        .filter(p =>
+          String(p.street.toLowerCase()).includes(
+            String(searchPhrase.toLowerCase()),
+          ),
         ),
-      )}
+    },
+    {
+      title: 'Krzyki',
+      data: points
+        .filter(p => p.sector.includes('2'))
+        .filter(p =>
+          String(p.street.toLowerCase()).includes(
+            String(searchPhrase.toLowerCase()),
+          ),
+        ),
+    },
+    {
+      title: 'Fabryczna',
+      data: points
+        .filter(p => p.sector.includes('3'))
+        .filter(p =>
+          String(p.street.toLowerCase()).includes(
+            String(searchPhrase.toLowerCase()),
+          ),
+        ),
+    },
+    {
+      title: 'Psie Pole',
+      data: points
+        .filter(p => p.sector.includes('4'))
+        .filter(p =>
+          String(p.street.toLowerCase()).includes(
+            String(searchPhrase.toLowerCase()),
+          ),
+        ),
+    },
+  ];
+
+  return (
+    <SectionList
+      sections={data}
       showsHorizontalScrollIndicator={false}
       showsVerticalScrollIndicator={false}
       renderItem={({item}) => renderItem(item)}
+      renderSectionHeader={({section: {title}}) => <HugeText>{title}</HugeText>}
       keyExtractor={item => item.pointId}
       style={{width: '100%'}}
     />
