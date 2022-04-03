b26c9e55516202281bfbbd024b1d14c08339fc9d
Matyjash <lengom4w@gmail.com>
2022-04-03 21:46:14 +0200

    Added new test notification for demo

--- mobile/src/screens/HelloSzopScreen.js
+++ mobile/src/screens/HelloSzopScreen.js
@@ -4,6 +4,7 @@ import {LanguageContext} from '../context/LanguageContextProvider';
 import {ScreenWrapper} from '../components/Wrapper';
 import {HomeCard} from '../components/HomeCard';
 import {HomeBanner} from '../components/HomeBanner';
+import {TextInput, Button, Alert} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 export const helloSzopScreenName = 'HelloSzopScreen';
 
@@ -14,6 +15,10 @@ const StyledScrollView = styled.ScrollView`
 export default function HelloSzopScreen() {
   const {text, selectedLanguage, setSelectedLanguage} =
     useContext(LanguageContext);
+
+  const [input, setInput] = React.useState();
+  const [input2, setInput2] = React.useState();
+
   return (
     <ScreenWrapper home>
       <StyledScrollView
@@ -25,6 +30,53 @@ export default function HelloSzopScreen() {
         <HomeCard />
         <HomeCard />
         <HomeCard last />
+
+        {/* Tylko do demo */}
+        <TextInput
+          placeholder="Godzina"
+          keyboardType="numeric"
+          value={input2}
+          onChangeText={setInput2}
+          style={{backgroundColor: 'white'}}
+        />
+        <TextInput
+          placeholder="Minuty"
+          keyboardType="numeric"
+          value={input}
+          onChangeText={setInput}
+          style={{backgroundColor: 'white'}}
+        />
+        <Button
+          title={'Test powiadomienia'}
+          onPress={() => {
+            Alert.alert(
+              'Dodano powiadomienie na godzine:',
+              new Date(
+                2022,
+                3,
+                4,
+                Number(input2) - 2,
+                Number(input),
+                0,
+                0,
+              ).toString(),
+            );
+            PushNotification.localNotificationSchedule({
+              channelId: 'szop-nt',
+              title: 'Test', // (optional)
+              message: 'Powiadomienie testowe', // (required)
+              date: new Date(
+                2022,
+                3,
+                4,
+                Number(input2) - 2,
+                Number(input),
+                0,
+                0,
+              ),
+            });
+          }}
+        />
       </StyledScrollView>
       {/*<HugeText>{text.appName}</HugeText>
       <Button
@@ -33,19 +85,7 @@ export default function HelloSzopScreen() {
           setSelectedLanguage(selectedLanguage == 'en' ? 'pl' : 'en')
         }
       />
-      <Button
-        title={'Test powiadomienia'}
-        onPress={() =>{
-          PushNotification.localNotificationSchedule({
-            channelId: "szop-nt",
-            title: "Test", // (optional)
-            message: "Powiadomienie testowe", // (required)
-            date: new Date(Date.now() + 10 * 1000), // in 10 sec
-            allowWhileIdle: true, 
-
-        });}
-        }
-      />*/}
+      */}
     </ScreenWrapper>
   );
 }
