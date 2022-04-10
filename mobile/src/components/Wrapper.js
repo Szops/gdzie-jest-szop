a6b229704f041406268558b90772805865b3bd55
StawKarpi <stanislaw.karpisiak@gmail.com>
2022-04-11 00:49:33 +0200

    Added accordion list with a section for enabled points

--- mobile/src/components/Wrapper.js
+++ mobile/src/components/Wrapper.js
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
+import {darkTileColor} from '../constants/colors';
 
 export const CardWrapper = styled.View`
-  padding: 10px 0 10px 0;
+  padding: 10px 100px 10px 0px;
   margin: 8px 10px 8px 10px;
   width: 90%;
   left: 10%;
   flex-direction: row;
   justify-content: space-between;
+`;
+
+export const SectionWrapper = styled.View`
+  padding: 10px 0px 10px 0px;
+  margin: 0px 139px 0px 0px;
+  flex-direction: row;
+  justify-content: center;
+  background-color: ${darkTileColor};
+  border-radius: 10px;
 `;
 
 export const ScreenWrapper = styled.View`
