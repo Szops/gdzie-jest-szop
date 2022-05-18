c0be4a20ba3402132e98a5ee39805b98f15d511b
Matyjash <61912671+Matyjash@users.noreply.github.com>
2022-05-18 13:30:00 +0200

    Simple notifications personalization and point highlight fix (#84)

--- mobile/src/utils/NotificationManager.js
+++ mobile/src/utils/NotificationManager.js
@@ -1,10 +1,15 @@
 import {View, Button} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useContext} from 'react';
 import PointsDAO from '../database/PointsDAO';
 import withObservables from '@nozbe/with-observables';
 import PushNotification from 'react-native-push-notification';
+import {NotificationContext} from '../context/NotificationContextProvider';
 
 const NotificationManager = ({points}) => {
+  const {offset} = useContext(NotificationContext);
+  const {muted} = useContext(NotificationContext);
+  console.log(offset);
+  console.log(muted);
   const syncNotifications = () => {
     PushNotification.cancelAllLocalNotifications();
     points
@@ -20,11 +25,11 @@ const NotificationManager = ({points}) => {
                   title: 'Przypomnienie o dostępności punktu',
                   message:
                     'Punkt ' + point.street + ' będzie wkrótce dostępny!',
-                  date: new Date(Number(date.date) - 3600000), //godzine wcześniej
+                  date: new Date(Number(date.date) - offset * 60000), //godzine wcześniej
                   allowWhileIdle: true,
                   soundName: 'notification_sound.wav',
                   sound: 'notification_sound.wav',
-                  playSound: true,
+                  playSound: !muted,
                   vibrate: true,
                 });
                 PushNotification.localNotificationSchedule({
@@ -35,7 +40,7 @@ const NotificationManager = ({points}) => {
                   allowWhileIdle: true,
                   soundName: 'notification_sound.wav',
                   sound: 'notification_sound.wav',
-                  playSound: true,
+                  playSound: !muted,
                   vibrate: true,
                 });
               }
