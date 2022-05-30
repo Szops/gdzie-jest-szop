739d61598e5603465de38ddb3f4ed87ec41a880f
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2022-05-30 17:09:17 +0200

    Add styles to modules list

--- mobile/src/components/LicensesList.js
+++ mobile/src/components/LicensesList.js
@@ -1,26 +1,36 @@
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity, Linking, Alert} from 'react-native';
 import React from 'react';
 import {TileHeader, TileText} from './Text';
 import * as licensesJSON from '../constants/licenses.json';
+import styled from 'styled-components';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
-const parsedLicenses = Object.entries(licensesJSON).map(([name, data]) => ({
-  name,
-  ...data,
-}));
+const parsedLicenses = Object.entries(licensesJSON)
+  .map(([name, data]) => ({
+    name,
+    ...data,
+  }))
+  .filter(license => license.name !== 'default');
 
 const LicensesList = () => {
-  const renderItem = item => (
-    <View>
-      <TileText>{item.name}</TileText>
-    </View>
-  );
+  const handleOpenInBrowser = url => {
+    Linking.openURL(url).catch(err => Alert.err("Couldn't load page", err));
+  };
   return (
     <View>
-      <TileHeader>Licenses:</TileHeader>
-      {parsedLicenses.map(license => (
-        <TouchableOpacity onPress={() => openBrowserAsync(license.licenseUrl)}>
-          <TileText>{license.name}</TileText>
-        </TouchableOpacity>
+      <TileHeader>USED MODULES</TileHeader>
+      {parsedLicenses.map((license, idx) => (
+        <Icon.Button
+          key={idx}
+          name={'sc-github'}
+          color={'white'}
+          size={32}
+          onPress={() => handleOpenInBrowser(license.repository)}
+          backgroundColor={'darkTileColor'}
+          underlayColor={'darkTileColor'}
+          iconStyle={{marginRight: 5}}>
+          {license.name}
+        </Icon.Button>
       ))}
     </View>
   );
